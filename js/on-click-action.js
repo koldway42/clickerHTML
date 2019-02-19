345b6e76b9d447aed6b2977cc0dbf4f6729afa8b
Koldway42 <math12125.jr@gmail.com>
2019-02-18 23:04:25 -0300

    Novo level, 5 novas armas, armas podem ter atributos especiais, ainda estão desbalanceadas, ultima armas está sem atributo, introdução da primra bossbattle

--- js/on-click-action.js
+++ js/on-click-action.js
@@ -17,13 +17,16 @@ new CriarEspada("Espada de Ferro Meteórico", 32, 2400, 4),
 new CriarEspada("Espada Lendária", 64, 5000, 5),
 ]
 
+const rnd = (min = 0, max = 100) => {
+    return Math.floor(Math.random() * (max - min) + min)
+}
 
 
 player = new Object
     player.nome = "Koldway42"
     player.arma = espadas[0]
     player.atk = player.arma.atk
-    player.dinheiro = 0
+    player.dinheiro = 0;
 
 const show_status = () => {
     document.getElementById("clicks").innerHTML = `Dinheiro: ${player.dinheiro}`
@@ -33,9 +36,16 @@ const show_status = () => {
 show_status();
 const button = () => {
     document.getElementById("click-GUI").onclick = (e) => {
+        let random = rnd();
         player.dinheiro = player.dinheiro + player.atk;
         document.getElementById("clicks").innerHTML = `Dinheiro: ${player.dinheiro}`
-        
+        if(player.arma.nome === "Espada de Material Não Confiavel" && random <= 15){
+            player.dinheiro = player.dinheiro + player.arma.atk;
+            console.log("x2")
+        }else if(player.arma.nome === "Espada de Thundera" && random <= 50){
+            player.dinheiro = player.dinheiro + (player.arma.atk * 2);
+            console.log("x3")
+        }
     }
 }
 button()
@@ -54,24 +64,40 @@ const compra_de_arma = () => {
         }
     }
     espadas_html[1].onclick = (e) => {
-        if(espadas[2].preco < player.dinheiro){
+        if(espadas[2].preco < player.dinheiro && player.arma.nome !== "Espada de Aspirante a Aventureiro"){
             player.arma = espadas[2]
             player.dinheiro = player.dinheiro - espadas[2].preco
             button()
             show_status()
             player.atk = player.arma.atk
             espadas_html[1].classList.add("desativar")
+        }else if(espadas[2].preco < player.dinheiro && player.arma.nome === "Espada de Aspirante a Aventureiro"){
+            player.arma = espadas[2]
+            player.dinheiro = player.dinheiro - espadas[2].preco
+            button()
+            show_status()
+            player.atk = player.arma.atk
+            alert("A espada de material não confiável começa a brilhar, aparentemente ela tem um encantamento onde o usuario tem 15% de chance por clique de ganhar do dobro que ele ganharia normalmente.")
+            espadas_html[1].classList.add("desativar")
         }else {
             alert("Você nao tem dinheiro para isso")
         }
     }
     espadas_html[2].onclick = (e) => {
-        if(espadas[3].preco < player.dinheiro){
+        if(espadas[3].preco < player.dinheiro && player.arma.nome !== "Espada de Material Não Confiavel"){
             player.arma = espadas[3]
             player.dinheiro = player.dinheiro - espadas[3].preco
             button()
             show_status()
             player.atk = player.arma.atk
+            espadas_html[2].classList.add("desativar")
+        }else if(espadas[3].preco < player.dinheiro && player.arma.nome === "Espada de Material Não Confiavel"){
+            player.arma = espadas[3]
+            player.dinheiro = player.dinheiro - espadas[3].preco
+            button()
+            show_status()
+            player.atk = player.arma.atk
+            alert("Sua espada de material não confiável começa a brilhar com sua nova aquisição, ela se põe no ar e é logo em seguida absorvida pela espada de Thundera que então herda seu encantamento, mas o encantamento acabou sofrendo um melhoramento agora tendo 50% de chance de dar o TRIPLO de dano")
             espadas_html[2].classList.add("desativar")
         }else{
             alert("Você nao tem dinheiro para isso")
@@ -84,23 +110,39 @@ const compra_de_arma = () => {
             button()
             show_status()
             player.atk = player.arma.atk
-            espadas_html[3].style = "display: none; transition: 0.5s"
+            espadas_html[3].classList.add("desativar")
         }else{
             alert("Você nao tem dinheiro para isso")
         }
     }
     espadas_html[4].onclick = (e) => {
-        if(espadas[5].preco < player.dinheiro){
+        if(espadas[5].preco < player.dinheiro && player.arma.nome === "Espada de Ferro Meteórico"){
             player.arma = espadas[5]
             player.dinheiro = player.dinheiro - espadas[5].preco
             button()
             show_status()
             player.atk = player.arma.atk
-            espadas_html[4].style = "display: none; transition: 0.5s"
+            espadas_html[4].classList.add("desativar")
             level2()
             level2_recriar()
             alert("Você alcançou um novo patamar, nenhum ser mortal pode mais te derrotar. Você agora arranja uma briga com deuses!!!")
-        }else{
+        }else if(espadas[5].preco < player.dinheiro && player.arma.nome === "Espada do Demonio Temporal"){
+            player.arma = espadas[5]
+            player.dinheiro = player.dinheiro - espadas[5].preco
+            button()
+            show_status()
+            player.atk = player.arma.atk
+            espadas_html[4].classList.add("desativar")
+            level3()
+            level3_recriar()
+            alert("Após derrotar todos os Deuses, você descobre o mundo onde você vivia era na verdade uma fração mínima do mundo real, e esses deuses que você derrotou, na realidade eram considerados seres muito fracos nesse outro mundo. Então você parte para uma aventura para crescer nessa dimensão superior.")
+        }else if(espadas[5].preco < player.dinheiro && player.arma.nome === "Espada de Tyr"){
+            espadas_html[4].classList.add("desativar")
+            alert("Após alguns anos nesse mundo, você se torna alguem conhecido e de muita influência venerado por muitos como o Deus do Novo mundo. Com isso você chama a atenção de Kratos, que logo começa a querer sua cabeça.")
+            alert("Alguns Dias depois, alguém arromba as portas de sua casa. Kratos esta te desafiando, aceitar o desafio?")
+            const kratos = document.querySelector("[Desafio]")
+            kratos.style = "display: block;"
+        }else {
             alert("Você nao tem dinheiro para isso")
         }
     }
@@ -111,7 +153,7 @@ function level2_recriar(){
     new CriarEspada("Espada de Divindade", 128, 15000, 1),
     new CriarEspada("Espada da Tartaruga Espiritual", 256, 50000, 2),
     new CriarEspada("Espada dos Mil Caminhos", 512, 100000, 3),
-    new CriarEspada("Espada do Demonio Temporal", 2048, 400000, 4),
+    new CriarEspada("Espada do Demonio Temporal", 2048, 300000, 4),
     new CriarEspada("Espada do Deus Imperador", 4096, 500000, 5))
     while(espadas.length > 6){
         espadas.shift()
@@ -151,3 +193,61 @@ function level2(){
     div_pai.appendChild(espada_do_deus_imperador)
     compra_de_arma()
 }
+function level3_recriar(){
+    espadas.push(new CriarEspada("Espada do Deus Imperador", 4096, 500000, 5),
+    new CriarEspada("Espada de Aspirante a Aventureiro", 8196, 750000, 1),
+    new CriarEspada("Espada de Material Não Confiavel", 24588, 1500000, 2),
+    new CriarEspada("Espada de Thundera", 49176, 3000000, 3),
+    new CriarEspada("Espada de Tyr", 98352, 6000000, 4),
+    new CriarEspada("Blades of Chaos", 491760, 12000000, 5))
+    while(espadas.length > 6){
+        espadas.shift()
+    }
+}
+function level3(){
+    const espadas = document.querySelectorAll("[espada]")
+    const div_pai = document.querySelector(".player")
+    espadas.forEach((e) => {
+        div_pai.removeChild(e)
+    })
+    
+    const espada_de_aventureiro_aspirante = document.createElement("div")
+    espada_de_aventureiro_aspirante.setAttribute("espada", "")
+    espada_de_aventureiro_aspirante.innerHTML = "Espada de Aspirante a Aventureiro"
+    
+    const espada_de_material_nao_confiavel = document.createElement("div")
+    espada_de_material_nao_confiavel.setAttribute("espada", "")
+    espada_de_material_nao_confiavel.innerHTML = "Espada de Material Não Confiavel"
+    
+    const espada_de_thundera = document.createElement("div")
+    espada_de_thundera.setAttribute("espada", "")
+    espada_de_thundera.innerHTML = "Espada de Thundera"
+    
+    const espada_de_tyr = document.createElement("div")
+    espada_de_tyr.setAttribute("espada", "")
+    espada_de_tyr.innerHTML = "Espada de Tyr"
+
+    const Blades_of_Chaos = document.createElement("div")
+    Blades_of_Chaos.setAttribute("espada","")
+    Blades_of_Chaos.innerHTML = "Blades of Chaos"
+
+    div_pai.appendChild(espada_de_aventureiro_aspirante)
+    div_pai.appendChild(espada_de_material_nao_confiavel)
+    div_pai.appendChild(espada_de_thundera)
+    div_pai.appendChild(espada_de_tyr)
+    div_pai.appendChild(Blades_of_Chaos)
+    compra_de_arma()
+}
+function Desafio(){
+    const sim = document.querySelector("[SIM]")
+    const nao = document.querySelector("[NAO]")
+    sim.onclick = () => {
+        alert("Você aceita o desafio de Kratos que logo empunha sua Blades of Chaos e parte para cima de você")
+        alert("Falta eu fazer o resto ;-;")
+    }
+    nao.onclick = () => {
+        alert("Kratos decide partir para cima de você da qualquer forma. Você não tem escolha")
+        alert("Falta eu fazer o resto ;-;") 
+    }
+}
+Desafio()
